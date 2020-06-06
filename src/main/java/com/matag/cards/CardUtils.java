53c51e4c66a9046320d996265fc3f0eeae87beb4
Antonio Alonzi <antonioalonzi85@gmail.com>
2020-06-06 19:02:10 +0100

    Moved out few methods from CardUtils.

--- src/main/java/com/matag/cards/CardUtils.java
+++ src/main/java/com/matag/cards/CardUtils.java
@@ -1,31 +1,13 @@
 package com.matag.cards;
 
 import com.matag.cards.properties.Color;
-import com.matag.cards.properties.Rarity;
 import com.matag.cards.properties.Type;
 
-import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
 
-import static com.matag.cards.ability.type.AbilityType.TAP_ADD_MANA;
 import static com.matag.cards.properties.Cost.COLORLESS;
-import static java.util.Collections.emptyList;
 
 public class CardUtils {
-  public static Card hiddenCard() {
-    return new Card("card", "/img/card-back.jpg", new TreeSet<>(), emptyList(), new TreeSet<>(), new TreeSet<>(), Rarity.COMMON, "", 0, 0, emptyList());
-  }
-
-  public static List<Color> colorsOfManaThatCanGenerate(Card card) {
-    return card.getAbilities().stream()
-      .filter(ability -> ability.getAbilityType().equals(TAP_ADD_MANA))
-      .flatMap(ability -> ability.getParameters().stream())
-      .map(Color::valueOf)
-      .collect(Collectors.toList());
-  }
-
   public static boolean isColorless(Card card) {
     return card.getCost().stream().noneMatch(cost -> cost != COLORLESS);
   }
