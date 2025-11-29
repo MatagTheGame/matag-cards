272dd35e18a733fa7b973425adc1f1b9f447d801
Antonio Alonzi <antonioalonzi85@gmail.com>
2025-11-29 21:54:51 +0000

    kotlin

--- src/main/java/com/matag/cards/Card.kt
+++ src/main/java/com/matag/cards/Card.kt
@@ -1,53 +1,19 @@
-package com.matag.cards;
+package com.matag.cards
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.TreeSet;
+import com.matag.cards.ability.Ability
+import com.matag.cards.properties.*
+import java.util.*
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
-import com.matag.cards.ability.Ability;
-import com.matag.cards.properties.Color;
-import com.matag.cards.properties.Cost;
-import com.matag.cards.properties.Rarity;
-import com.matag.cards.properties.Subtype;
-import com.matag.cards.properties.Type;
-
-import lombok.Builder;
-import lombok.Value;
-
-@Value
-@JsonDeserialize(builder = Card.CardBuilder.class)
-@Builder(toBuilder = true)
-public class Card {
-  String name;
-  String imageUrl;
-  TreeSet<Color> colors;
-  List<Cost> cost;
-  TreeSet<Type> types;
-  TreeSet<Subtype> subtypes;
-  Rarity rarity;
-  String ruleText;
-  int power;
-  int toughness;
-  List<Ability> abilities;
-
-  public Card(String name, String imageUrl, TreeSet<Color> colors, List<Cost> cost, TreeSet<Type> types,
-              TreeSet<Subtype> subtypes, Rarity rarity, String ruleText, int power, int toughness, List<Ability> abilities) {
-    this.name = name;
-    this.imageUrl = imageUrl;
-    this.colors = colors != null ? colors : new TreeSet<>();
-    this.cost = cost != null ? cost : new ArrayList<>();
-    this.types = types != null ? types : new TreeSet<>();
-    this.subtypes = subtypes != null ? subtypes : new TreeSet<>();
-    this.rarity = rarity;
-    this.ruleText = ruleText != null ? ruleText : "";
-    this.power = power;
-    this.toughness = toughness;
-    this.abilities = abilities != null ? abilities : new ArrayList<>();
-  }
-
-  @JsonPOJOBuilder(withPrefix = "")
-  public static class CardBuilder {}
-
-}
+data class Card(
+    val name: String,
+    val imageUrl: String,
+    val colors: TreeSet<Color?>,
+    val cost: List<Cost>,
+    val types: TreeSet<Type>,
+    val subtypes: TreeSet<Subtype>,
+    var rarity: Rarity,
+    val ruleText: String,
+    val power: Int,
+    val toughness: Int,
+    val abilities: MutableList<Ability>
+)
--- src/test/java/com/matag/cards/LinkerTest.kt
+++ src/test/java/com/matag/cards/LinkerTest.kt
@@ -1,92 +1,94 @@
-package com.matag.cards;
+package com.matag.cards
 
-import static com.matag.cards.properties.Type.BASIC;
-import static java.util.stream.Collectors.toList;
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.core.util.DefaultIndenter
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.matag.cards.properties.Type
+import com.matag.cards.sets.MtgSets
+import com.matag.downloader.CardScryFallLinker
+import org.junit.Ignore
+import org.junit.Test
+import org.junit.platform.commons.util.StringUtils
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.junit4.SpringRunner
+import java.nio.file.Files
+import java.nio.file.Paths
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
+@RunWith(SpringRunner::class)
+@Import(CardsConfiguration::class)
+class LinkerTest {
+    @Autowired
+    private val cards: Cards? = null
 
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.platform.commons.util.StringUtils;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit4.SpringRunner;
+    @Autowired
+    private val mtgSets: MtgSets? = null
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.util.DefaultIndenter;
-import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.matag.cards.sets.MtgSets;
-import com.matag.downloader.CardScryFallLinker;
+    @Ignore
+    @Test
+    @Throws(Exception::class)
+    fun scryFallLinker() {
+        val cardsObjectMapper = createCardsObjectMapper()
+        val setsObjectMapper = createSetsObjectMapper()
 
-@RunWith(SpringRunner.class)
-@Import(CardsConfiguration.class)
-public class LinkerTest {
+        val sets = mtgSets!!.getSets()
 
-  @Autowired
-  private Cards cards;
+        val cardsToLink = cards!!.getAll().stream()
+            .filter { card: Card? -> StringUtils.isBlank(card!!.imageUrl) }
+            .toList()
 
-  @Autowired
-  private MtgSets mtgSets;
+        for (i in cardsToLink.indices) {
+            val card: Card = cardsToLink.get(i)
+            val cardScryFallLinker = CardScryFallLinker(card)
+            val updatedCard = card.copy(
+                    imageUrl = cardScryFallLinker.getImage(),
+                    types = cardScryFallLinker.getTypes(),
+                    subtypes = cardScryFallLinker.getSubtypes(),
+                    power = cardScryFallLinker.getPower(),
+                    toughness = cardScryFallLinker.getToughness(),
+                    rarity = cardScryFallLinker.getRarity(),
+                    ruleText = cardScryFallLinker.getOracleText(),
+                    colors = cardScryFallLinker.getColors(),
+                    cost = cardScryFallLinker.getManaCost()
+                )
+            val cardJson = cardsObjectMapper.writeValueAsString(updatedCard)
+            Files.writeString(
+                Paths.get(CardsConfiguration.getResourcesPath() + "/cards/" + card.name + ".json"),
+                cardJson
+            )
+            println("Downloaded " + (i + 1) + " of " + cardsToLink.size + " -> " + card.name)
 
-  @Ignore
-  @Test
-  public void scryFallLinker() throws Exception {
-    var cardsObjectMapper = createCardsObjectMapper();
-    var setsObjectMapper = createSetsObjectMapper();
-
-    var sets = mtgSets.getSets();
-
-    var cardsToLink = cards.getAll().stream()
-      .filter(card -> StringUtils.isBlank(card.getImageUrl()))
-      .collect(toList());
-
-    for (int i = 0; i < cardsToLink.size(); i++) {
-      var card = cardsToLink.get(i);
-      var cardScryFallLinker = new CardScryFallLinker(card);
-      var updatedCard = card.toBuilder()
-          .imageUrl(cardScryFallLinker.getImage())
-          .types(cardScryFallLinker.getTypes())
-          .subtypes(cardScryFallLinker.getSubtypes())
-          .power(cardScryFallLinker.getPower())
-          .toughness(cardScryFallLinker.getToughness())
-          .rarity(cardScryFallLinker.getRarity())
-          .ruleText(cardScryFallLinker.getOracleText())
-          .colors(cardScryFallLinker.getColors())
-          .cost(cardScryFallLinker.getManaCost())
-          .build();
-      var cardJson = cardsObjectMapper.writeValueAsString(updatedCard);
-      Files.writeString(Paths.get(CardsConfiguration.getResourcesPath() + "/cards/" + card.getName() + ".json"), cardJson);
-      System.out.println("Downloaded " + (i + 1) + " of " + cardsToLink.size() + " -> " + card.getName());
-
-      if (!card.getTypes().contains(BASIC)) {
-        for (String scryFallSet : cardScryFallLinker.getSets()) {
-          if (sets.containsKey(scryFallSet)) {
-            sets.get(scryFallSet).getCards().add(card.getName());
-            var setJson = setsObjectMapper.writeValueAsString(sets.get(scryFallSet));
-            Files.writeString(Paths.get(CardsConfiguration.getResourcesPath() + "/sets/" + scryFallSet + ".json"), setJson);
-          }
+            if (!card.types.contains(Type.BASIC)) {
+                for (scryFallSet in cardScryFallLinker.getSets()) {
+                    if (sets.containsKey(scryFallSet)) {
+                        sets.get(scryFallSet)!!.getCards().add(card.name)
+                        val setJson = setsObjectMapper.writeValueAsString(sets.get(scryFallSet))
+                        Files.writeString(
+                            Paths.get(CardsConfiguration.getResourcesPath() + "/sets/" + scryFallSet + ".json"),
+                            setJson
+                        )
+                    }
+                }
+            }
         }
-      }
     }
-  }
 
-  public ObjectMapper createCardsObjectMapper() {
-    var objectMapper = new ObjectMapper();
-    objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-    objectMapper.setDefaultPropertyInclusion(JsonInclude.Include.NON_DEFAULT);
-    return objectMapper;
-  }
+    fun createCardsObjectMapper(): ObjectMapper {
+        val objectMapper = ObjectMapper()
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT)
+        objectMapper.setDefaultPropertyInclusion(JsonInclude.Include.NON_DEFAULT)
+        return objectMapper
+    }
 
-  public ObjectMapper createSetsObjectMapper() {
-    var objectMapper = new ObjectMapper();
-    objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-    DefaultPrettyPrinter prettyPrinter = new DefaultPrettyPrinter();
-    prettyPrinter.indentArraysWith(DefaultIndenter.SYSTEM_LINEFEED_INSTANCE);
-    objectMapper.setDefaultPrettyPrinter(prettyPrinter);
-    return objectMapper;
-  }
+    fun createSetsObjectMapper(): ObjectMapper {
+        val objectMapper = ObjectMapper()
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT)
+        val prettyPrinter = DefaultPrettyPrinter()
+        prettyPrinter.indentArraysWith(DefaultIndenter.SYSTEM_LINEFEED_INSTANCE)
+        objectMapper.setDefaultPrettyPrinter(prettyPrinter)
+        return objectMapper
+    }
 }
